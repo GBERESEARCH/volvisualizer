4868cdd0e52827bde043f92004c2da9f09076799
GBERESEARCH <68221707+GBERESEARCH@users.noreply.github.com>
2020-10-06 10:08:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,38 @@
-from setuptools import setup
+import pathlib
+from setuptools import setup, find_packages
 
-setup(name='volatility',
-      version='0.0.1',
-      description='Extract and visualize implied volatility from option chain data',
-      author='...',
-      author_email='...',
-      packages=['volatility', 'models'],
-      install_requires=['matplotlib',
-			'numpy',
-                        'plotly',
-			'scipy',
-			'pandas',
-			'requests',
-			'pytz'])
+HERE = pathlib.Path(__file__).parent
 
+VERSION = '0.1.0'
+PACKAGE_NAME = 'volvisualizer'
+AUTHOR = '...'
+AUTHOR_EMAIL = '...'
+URL = 'https://github.com/GBERESEARCH/volvisualizer'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'Extract and visualize implied volatility from option chain data.'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'matplotlib',
+      'numpy',
+      'plotly',
+      'scipy',
+      'pandas',
+      'requests',
+      'pytz'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
